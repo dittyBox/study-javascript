edeb007eefe93b693d711f3acae096a021bfacd4
ditty <ttp12345@naver.com>
2021-01-08 20:02:19 +0900

    study egoing javascript

--- modal.js
+++ modal.js
@@ -1,2 +1,25 @@
 //바닐라 스크립트로 모달창 띄우기
-//흠냐
+//노마드 코더님의 로드맵 내용중에 바닐라 JS로 각각의 기술을 
+//라이브러리 도움 없이 개발 하라는 거에서 즉흥적으로 시도
+//근데.... CSS 공부해야 할듯... 잰장....
+
+let modalDiv = "<div>이런식으로</div>";
+
+/*
+<div class="black_bg"></div>
+<div class="modal_wrap">
+    <div class="modal_close"><a href="#">close</a></div>
+    <div>
+        모달창 내용
+    </div>
+<div>
+*/
+
+
+
+
+let rootDiv = document.getElementById("root");
+
+
+rootDiv.innerHTML = modalDiv;
+
