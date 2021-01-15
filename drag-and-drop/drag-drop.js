158e6126be78b67fc4d7fc9dc6bcfacd8af33777
DittyBOX <76231751+dittyBox@users.noreply.github.com>
2021-01-15 14:00:12 +0900

    Update drag-drop.js

--- drag-and-drop/drag-drop.js
+++ drag-and-drop/drag-drop.js
@@ -1,6 +1,13 @@
-<div class='parent'>
+
+
+let modalDiv = `<div class='parent'>
   <span id='draggableSpan'>
     draggable
   </span>
   <span> dropzone </span>
-</div>
+</div>`;
+
+let rootDiv = document.getElementById("root");
+
+
+rootDiv.innerHTML = modalDiv;
