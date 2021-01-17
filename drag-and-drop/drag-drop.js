035fe12a63b42c68391119269ab81ad8387a85a0
ditty <ttp12345@naver.com>
2021-01-17 22:52:35 +0900

    add drag and drop

--- drag-and-drop/drag-drop.js
+++ drag-and-drop/drag-drop.js
@@ -1,13 +1,60 @@
 
+function draggable(obj,title){
+  let draggableItem = document.createElement('div');
+  draggableItem.classList.add('draggable');
+  draggableItem.innerText = `${title}`;
+  draggableItem.setAttribute('id',`${title}`);
+  draggableItem.setAttribute('draggable','true');
+  draggableItem.setAttribute('ondragstart','onDragStart(event);');
 
-let modalDiv = `<div class='parent'>
-  <span id='draggableSpan'>
-    draggable
-  </span>
-  <span> dropzone </span>
-</div>`;
+  obj.appendChild(draggableItem);
+}
+
+function onDragStart(event) {
+  event
+    .dataTransfer
+    .setData('text/plain', event.target.id);
+
+  event
+    .currentTarget
+    .style
+    .backgroundColor = 'yellow';
+}
+
+function onDragOver(event) {
+  event.preventDefault();
+}
+
+function onDrop(event) {
+  const id = event
+    .dataTransfer
+    .getData('text');
+    const draggableElement = document.getElementById(id);
+    const dropzone = event.target;
+    dropzone.appendChild(draggableElement);
+    event
+      .dataTransfer
+      .clearData();
+}
+
 
 let rootDiv = document.getElementById("root");
+rootDiv.classList.add('root-parent');
 
+let origin = document.createElement('div');
+origin.classList.add('origin');
 
-rootDiv.innerHTML = modalDiv;
+let dropZone = document.createElement('div');
+dropZone.classList.add('dropzone');
+dropZone.setAttribute('ondragover','onDragOver(event);');
+dropZone.setAttribute('ondrop','onDrop(event);');
+
+draggable(rootDiv,'item1');
+draggable(rootDiv,'item2');
+draggable(rootDiv,'item3');
+
+dropZone.innerText = 'DropZone';
+
+rootDiv.appendChild(origin);
+rootDiv.appendChild(dropZone);
+
---
+++ dropdown/app.js
@@ -0,0 +1,3 @@
+
+
+let divRoot = document.querySelector("#root");
