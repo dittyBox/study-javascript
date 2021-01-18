5965e9e7ec43a6410de48b38cbe436c7ccf6e160
DittyBOX <76231751+dittyBox@users.noreply.github.com>
2021-01-18 15:08:32 +0900

    Create index.js

---
+++ bridge/index.js
@@ -0,0 +1,37 @@
+var bridgecont = document.getElementById("app");
+
+var bardown = document.querySelectorAll(".bardown");
+var itemRight = document.querySelectorAll(".itemRight");
+
+bardown.forEach(function (item) {
+  item.addEventListener("click", function (e) {
+    // const dropdown = e.currentTarget.parentNode.querySelector(".dropdown");
+    // dropdown.classList.toggle("show");
+    bror(document.querySelector(".show"));
+    e.currentTarget.classList.toggle("show");
+
+    // const marker = e.currentTarget.querySelector(".marker");
+    // marker.classList.toggle("hide");
+    // console.log(e.currentTarget);
+  });
+});
+
+itemRight.forEach(function (item) {
+  item.addEventListener("click", function (e) {
+    // const dropdown = e.currentTarget.parentNode.querySelector(".dropdown");
+    // dropdown.classList.toggle("show");
+    bror(document.querySelector(".show"));
+    e.currentTarget.classList.toggle("show");
+
+    // const marker = e.currentTarget.querySelector(".marker");
+    // marker.classList.toggle("hide");
+    // console.log(e.currentTarget);
+  });
+});
+
+function bror(obj) {
+  if (obj) {
+    obj.classList.remove("show");
+    console.log(obj);
+  }
+}
